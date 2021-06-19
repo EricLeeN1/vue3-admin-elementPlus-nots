7369a4542312e9e8352103beba345a22c9c18304
EricLeeN1 <179310009@qq.com>
2021-06-19 19:23:14 +0800

    增加elementplus

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
