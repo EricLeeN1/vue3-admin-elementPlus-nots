ccb88ff38efa1bbadb9cdc2b115e50495b3ec31d
EricLeeN1 <179310009@qq.com>
2021-06-19 19:19:51 +0800

    增加基础路由模块

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
+// import Vuex from '@/views/vuex.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import('@/views/login/index.vue') // 懒加载组件
+  },
+  // {
+  //   path: '/vuex',
+  //   name: 'Vuex',
+  //   component: Vuex
+  // },
+  // {
+  //   path: '/axios',
+  //   name: 'Axios',
+  //   component: () => import('@/views/axios.vue') // 懒加载组件
+  // }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
