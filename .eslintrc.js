37532ad9d0d409bf7f27858bbf12214acac45724
EricLeeN1 <179310009@qq.com>
2021-06-19 19:44:55 +0800

    解决 Prettier 和 ESLint 的冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-  ],
-  rules: {
-  },
-};
+  plugins: ['vue'],
+  rules: {}
+}
