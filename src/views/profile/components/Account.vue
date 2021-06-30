841da15004a3790215da842e3770dcf2a3d11353
EricLeeN1 <179310009@qq.com>
2021-06-30 13:07:03 +0800

    feat(components): 增加profile页面

---
+++ src/views/profile/components/Account.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-form>
+    <el-form-item label="Name">
+      <el-input v-model.trim="userInfo.name" />
+    </el-form-item>
+    <el-form-item label="Email">
+      <el-input v-model.trim="userInfo.email" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submit">Update</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+import { ElMessage } from 'element-plus'
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => {
+        return {
+          name: '',
+          email: ''
+        }
+      }
+    }
+  },
+  setup(props) {
+    const submit = () => {
+      ElMessage({
+        message: 'User information has been updated successfully',
+        type: 'success',
+        duration: 5 * 1000
+      })
+    }
+    const state = reactive({
+      userInfo: props.user
+    })
+    return {
+      ...toRefs(state),
+      submit
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,11 +1,69 @@
 <template>
-  <div></div>
+  <div class="app-container">
+    <div v-if="user">
+      <el-row :gutter="20">
+        <el-col :span="6" :xs="24">
+          <user-card :user="user" />
+        </el-col>
+
+        <el-col :span="18" :xs="24">
+          <el-card>
+            <el-tabs v-model="activeTab">
+              <el-tab-pane label="Activity" name="activity">
+                <activity />
+              </el-tab-pane>
+              <el-tab-pane label="Timeline" name="timeline">
+                <timeline />
+              </el-tab-pane>
+              <el-tab-pane label="Account" name="account">
+                <account :user="user" />
+              </el-tab-pane>
+            </el-tabs>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
+import { reactive, toRefs, onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import UserCard from './components/UserCard.vue'
+import Activity from './components/Activity.vue'
+import Timeline from './components/Timeline.vue'
+import Account from './components/Account.vue'
+
 export default {
+  name: 'Profile',
+  components: { UserCard, Activity, Timeline, Account },
   setup() {
-    return {}
+    const store = useStore()
+    const state = reactive({
+      user: {},
+      activeTab: 'activity'
+    })
+    const name = computed(() => store.getters.name)
+    const avatar = computed(() => store.getters.avatar)
+    const roles = computed(() => store.getters.roles)
+    const getUser = () => {
+      state.user = {
+        name,
+        role: roles.value.join(' | '),
+        email: 'admin@test.com',
+        avatar
+      }
+    }
+    onMounted(() => {
+      getUser()
+    })
+    return {
+      ...toRefs(state),
+      name,
+      avatar,
+      roles,
+      getUser
+    }
   }
 }
 </script>
